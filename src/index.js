9ebc28e7cb89f0609cc0d2d4131d010b8d2757cf
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-14 21:03:40 +1000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2024 t2vee. All rights reserved.
+// Use of this source code is governed by an MPL license.
+
+// ITTY
+import {AutoRouter, cors, error} from 'itty-router'
+// HANDLERS
+import HandleSpotifyUserInfoEndpoint from './handlers/handleSpotifyUserInfoEndpoint'
+
+
+const { preflight, corsify } = cors({
+    origin: 'https://id.mxs.app',
+    credentials: true,
+    allowMethods: ['GET', 'POST'],
+    maxAge: 15000
+})
+const router = AutoRouter({
+    before: [preflight],
+    finally: [corsify],
+});
+
+router.get('/api/v1/oauth-user-info/endpoint/api-spotify-com/v1/me', HandleSpotifyUserInfoEndpoint)
+
+router
+    .all('*', () => error(404, 'this is not the route you are looking for'))
+
+export default { ...router }
