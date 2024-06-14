234b35586313b113209710f2029a2da408fd1044
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-14 21:23:34 +1000

    implemented twitch map

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 import {AutoRouter, cors, error} from 'itty-router'
 // HANDLERS
 import HandleSpotifyUserInfoEndpoint from './handlers/handleSpotifyUserInfoEndpoint'
+import HandleTwitchUserInfoEndpoint from "./handlers/handleTwitchUserInfoEndpoint";
 
 
 const { preflight, corsify } = cors({
@@ -18,7 +19,9 @@ const router = AutoRouter({
     finally: [corsify],
 });
 
-router.get('/api/v1/oauth-user-info/endpoint/api-spotify-com/v1/me', HandleSpotifyUserInfoEndpoint)
+router
+	.get('/api/v1/oauth-user-info/endpoint/api-spotify-com/v1/me', HandleSpotifyUserInfoEndpoint)
+	.get('/api/v1/oauth-user-info/endpoint/api-twitch-com/helix/users', HandleTwitchUserInfoEndpoint)
 
 router
     .all('*', () => error(404, 'this is not the route you are looking for'))
